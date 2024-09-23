0831516f6b2dc2f61dad7f4c8832c7eddb9b0b87
imasolodev <imasolodev@gmail.com>
2024-09-23 04:15:25 -0500

    Item Data Component

---
+++ Source/Demo/Private/Components/ItemComponent.cpp
@@ -0,0 +1,18 @@
+﻿#include "Demo/Public/Components/ItemComponent.h"
+
+UItemComponent::UItemComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+
+	static ConstructorHelpers::FObjectFinder<UDataTable> DataTableObj(TEXT("/Game/Data/ItemDataTable.ItemDataTable"));
+	if (DataTableObj.Succeeded())
+	{
+		ItemHandle.DataTable = DataTableObj.Object;
+		DataTable = DataTableObj.Object;
+	}
+}
+
+void UItemComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
---
+++ Source/Demo/Public/Components/ItemComponent.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "ItemComponent.generated.h"
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class DEMO_API UItemComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UItemComponent();
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	FDataTableRowHandle ItemHandle;
+
+	UPROPERTY()
+	TObjectPtr<UDataTable> DataTable;
+
+protected:
+	virtual void BeginPlay() override;
+	
+};
