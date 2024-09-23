7a865d2aedb5468bd2874e7594dd33cdafcdd476
imasolodev <imasolodev@gmail.com>
2024-09-23 04:25:53 -0500

    Base Item Class

---
+++ Source/Demo/Private/Items/BaseItem.cpp
@@ -0,0 +1,42 @@
+﻿#include "Demo/Public/Items/BaseItem.h"
+#include "Components/SphereComponent.h"
+#include "Demo/Data/ItemDataSet.h"
+#include "Demo/Public/Components/ItemComponent.h"
+
+ABaseItem::ABaseItem()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	ItemMesh = CreateDefaultSubobject<UStaticMeshComponent>("Item Mesh");
+	ItemMesh->SetCollisionResponseToChannel(ECC_Camera, ECR_Ignore);
+	ItemMesh->SetCollisionResponseToChannel(ECC_Pawn, ECR_Ignore);
+	SetRootComponent(ItemMesh);
+
+	ItemSphere = CreateDefaultSubobject<USphereComponent>("Item Sphere");
+	ItemSphere->SetSphereRadius(75.0f);
+	ItemSphere->SetLineThickness(1.0f);
+	ItemSphere->ShapeColor = FColor::Emerald;
+	ItemSphere->SetupAttachment(GetRootComponent());
+
+	ItemComponent = CreateDefaultSubobject<UItemComponent>("Item Component");
+}
+
+void ABaseItem::BeginPlay()
+{
+	Super::BeginPlay();
+
+	ItemMesh->SetSimulatePhysics(true);
+}
+
+void ABaseItem::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+
+	if (!ItemComponent->DataTable) { return; }
+
+	const FItemData* FoundData = ItemComponent->DataTable->FindRow<FItemData>(ItemComponent->ItemHandle.RowName, "");
+	if (!FoundData) { return; }
+
+	UStaticMesh* LoadedMesh = FoundData->AssetData.ItemMesh.LoadSynchronous();
+	ItemMesh->SetStaticMesh(LoadedMesh);
+}
---
+++ Source/Demo/Public/Items/BaseItem.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BaseItem.generated.h"
+
+class UItemComponent;
+class USphereComponent;
+
+UCLASS()
+class DEMO_API ABaseItem : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ABaseItem();
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void OnConstruction(const FTransform& Transform) override;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	TObjectPtr<UStaticMeshComponent> ItemMesh;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	TObjectPtr<USphereComponent> ItemSphere;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	TObjectPtr<UItemComponent> ItemComponent;
+
+};
